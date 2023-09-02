4e198884232fab36f6c6470561f988814558b1cb
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-09-02 12:41:16 +0200

    feat: add link

--- src/components/pokemon-card/pokemon-card.component.tsx
+++ src/components/pokemon-card/pokemon-card.component.tsx
@@ -1,18 +1,22 @@
 import React from "react"
 import Image from "next/image"
+import Link from "next/link"
+import { PokemonCard } from "@/domain/pokemon/pokemon-card.types"
 
-export const PokemonCard = ({ pokemon, index }) => {
+export const PokemonCardComponent = ({ pokemon, index }: PokemonCard) => {
   const pokemonIndex = ("000" + (index + 1)).slice(-3)
 
   return (
-    <div className="bg-cyan-950 rounded m-5 p-5 flex flex-col justify-center item-center">
-      <Image
-        width={200}
-        height={200}
-        src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonIndex}.png`}
-        alt={pokemon.name}
-      ></Image>
-      <div className="text-center">{pokemon.name}</div>
-    </div>
+    <Link href={`/pokemon-detail/${pokemon.name}`}>
+      <div className="bg-cyan-950 rounded m-5 p-5 flex flex-col justify-center item-center">
+        <Image
+          width={200}
+          height={200}
+          src={`https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${pokemonIndex}.png`}
+          alt={pokemon.name}
+        />
+        <div className="text-center">{pokemon.name}</div>
+      </div>
+    </Link>
   )
 }
