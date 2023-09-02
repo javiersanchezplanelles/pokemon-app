e94f1d58b273e4b9d8ee95b66b87bff28dc58a71
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-09-02 19:25:41 +0200

    feat: load more functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,34 @@ import { useRouter } from "next/router"
 import { SearchbarComponent } from "../components/searchbar/searchbar.component"
 import { Pokemon } from "../domain/pokemon/pokemon.types"
 
-export default function Home({ pokemonList }) {
+interface PokemonResponse {
+  pokemonList: {
+    count: number
+    next: string
+    previous: string
+    results: Pokemon[]
+  }
+}
+
+export default function Home({ pokemonList }: PokemonResponse) {
   const [pokemons, setPokemons] = useState(pokemonList)
   const [searchbarInput, setSearchbarInput] = useState("")
+
   const router = useRouter()
 
   const handleSearchbar = async () => {
     router.push(`/pokemon-detail/${searchbarInput}`)
+  }
+
+  const handleLoadMore = async (url: string, next: boolean) => {
+    const response = await fetch(url)
+    const nextPokemon = await response.json()
+    let updatedPokemonList = pokemons.results.concat(nextPokemon.results)
+    setPokemons({
+      ...pokemons,
+      next: nextPokemon.next,
+      results: updatedPokemonList,
+    })
   }
 
   return (
@@ -29,6 +50,12 @@ export default function Home({ pokemonList }) {
           />
         ))}
       </section>
+      <button
+        onClick={() => handleLoadMore(pokemons.next, true)}
+        className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
+      >
+        Load more
+      </button>
     </Layout>
   )
 }
