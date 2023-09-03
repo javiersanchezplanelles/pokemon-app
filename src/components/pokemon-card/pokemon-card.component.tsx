c7e53a78a31573f1a2ec9cc35932c346fcccd7cd
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-09-03 20:24:04 +0200

    feat: change fetch approach

--- src/components/pokemon-card/pokemon-card.component.tsx
+++ src/components/pokemon-card/pokemon-card.component.tsx
@@ -2,9 +2,15 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { PokemonCard } from "../../domain/pokemon/pokemon-card.types"
+import { PokemonType } from "@/domain/pokemon/pokemon.types"
 
 export const PokemonCardComponent = ({ pokemon, index }: PokemonCard) => {
   const pokemonIndex = ("000" + (index + 1)).slice(-3)
+  const renderTypes = () => {
+    return pokemon.types.map((typeInfo: PokemonType) => (
+      <li key={`${pokemonIndex}-${typeInfo.name}`}>{typeInfo.name}</li>
+    ))
+  }
 
   return (
     <Link href={`/pokemon-detail/${pokemon.name}`}>
@@ -16,6 +22,9 @@ export const PokemonCardComponent = ({ pokemon, index }: PokemonCard) => {
           alt={pokemon.name}
         />
         <div className="text-center">{pokemon.name}</div>
+        <div>
+          <ul className="bg-cyan-800 p-1 rounded">{renderTypes()}</ul>
+        </div>
         <div className="mt-5 text-center font-bold italic">
           <p>#{pokemonIndex}</p>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
 import Layout from "../components/layout/layout"
 import { PokemonCardComponent } from "../components/pokemon-card/pokemon-card.component"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/router"
 import { SearchbarComponent } from "../components/searchbar/searchbar.component"
 import { Pokemon } from "../domain/pokemon/pokemon.types"
-import { buildNextUrl, getOffsetFromUrl } from "../utils/url"
-
 interface PokemonResponse {
   pokemonList: {
     count: number
@@ -13,37 +11,32 @@ interface PokemonResponse {
     previous: string
     results: Pokemon[]
   }
+  pokemonDataList: any
 }
 
-export default function Home({ pokemonList }: PokemonResponse) {
+export default function Home({ pokemonDataList }: PokemonResponse) {
   const FIRST_POKEMON_GENERATION_COUNT = 151
   const MAX_PAGE_LIMIT = 20
-  const [offset, setOffset] = useState(getOffsetFromUrl(pokemonList.next))
-  const [pokemons, setPokemons] = useState(pokemonList)
+  const [offset, setOffset] = useState(0)
   const [searchbarInput, setSearchbarInput] = useState("")
-
   const router = useRouter()
 
   const handleSearchbar = async () => {
-    router.push(`/pokemon-detail/${searchbarInput}`)
+    router.push(`/pokemon-detail/${searchbarInput.toLowerCase()}`)
+  }
+  const [initialPokemonList, setInitialPokemonList] = useState(
+    pokemonDataList.slice(offset, 20)
+  )
+  const handleLoadMore = () => {
+    setInitialPokemonList([
+      ...initialPokemonList,
+      ...pokemonDataList.slice(offset, offset + MAX_PAGE_LIMIT),
+    ])
   }
 
-  const handleLoadMore = async (url: string) => {
-    const response = await fetch(url)
-    const nextPokemon = await response.json()
-    const updatedPokemonList = pokemons.results.concat(nextPokemon.results)
-    const currentOffset = getOffsetFromUrl(nextPokemon.next)
-    setOffset(currentOffset)
-    const pokemonToBeLoadedCount =
-      FIRST_POKEMON_GENERATION_COUNT - currentOffset
-    const currentPageLimit = Math.min(MAX_PAGE_LIMIT, pokemonToBeLoadedCount)
-
-    setPokemons({
-      ...pokemons,
-      next: buildNextUrl(nextPokemon.next, currentPageLimit),
-      results: updatedPokemonList,
-    })
-  }
+  useEffect(() => {
+    setOffset(initialPokemonList.length)
+  }, [initialPokemonList])
 
   return (
     <Layout title={"Pokemon list"}>
@@ -52,7 +45,7 @@ export default function Home({ pokemonList }: PokemonResponse) {
         onSearch={handleSearchbar}
       />
       <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5">
-        {pokemons.results.map((pokemon: Pokemon, index: number) => (
+        {initialPokemonList.map((pokemon: Pokemon, index: number) => (
           <PokemonCardComponent
             key={pokemon.name}
             pokemon={pokemon}
@@ -62,7 +55,7 @@ export default function Home({ pokemonList }: PokemonResponse) {
       </section>
       <div className="text-center mt-5">
         <button
-          onClick={() => handleLoadMore(pokemons.next)}
+          onClick={handleLoadMore}
           className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
           disabled={offset >= FIRST_POKEMON_GENERATION_COUNT}
         >
@@ -73,13 +66,38 @@ export default function Home({ pokemonList }: PokemonResponse) {
   )
 }
 
-export async function getStaticProps(context) {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon")
+export async function getStaticProps() {
+  const FIRST_POKEMON_GENERATION_COUNT = 151
+
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?limit=${FIRST_POKEMON_GENERATION_COUNT}`
+  )
   const pokemonList = await response.json()
+
+  const pokemonDataList = await Promise.all(
+    pokemonList.results.map(async (item) => {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${item.name}`
+      )
+      const pokemonJson = await response.json()
+
+      return {
+        name: pokemonJson.name,
+        url: "",
+        types: pokemonJson.types.map((item) => {
+          return {
+            slot: item.slot,
+            name: item.type.name,
+          }
+        }),
+      }
+    })
+  )
 
   return {
     props: {
       pokemonList,
+      pokemonDataList,
     },
   }
 }
