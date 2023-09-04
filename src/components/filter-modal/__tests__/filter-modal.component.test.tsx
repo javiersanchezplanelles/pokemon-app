824da68b5d20507dff9055d5a1ce55d40659bf01
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-09-04 20:16:57 +0200

    feat: add filter modal tests

---
+++ src/components/filter-modal/__tests__/filter-modal.component.test.tsx
@@ -0,0 +1,80 @@
+import React from "react"
+import { render, screen } from "@testing-library/react"
+import { FilterModalComponent } from "../filter-modal.component"
+import userEvent from "@testing-library/user-event"
+
+describe("FilterModalComponent", () => {
+  const onFind = jest.fn()
+  const onClear = jest.fn()
+  const setSelectedPokemonType = jest.fn()
+
+  it("should render a button", () => {
+    render(
+      <FilterModalComponent
+        onFind={onFind}
+        onClear={onClear}
+        setSelectedPokemonType={setSelectedPokemonType}
+      />
+    )
+    const filterModalButton = screen.getByRole("button", {
+      name: /filter by type/i,
+    })
+
+    expect(filterModalButton).toBeInTheDocument()
+  })
+
+  describe("upon clicking the button", () => {
+    const clickButton = () => {
+      const filterModalButton = screen.getByRole("button", {
+        name: /filter by type/i,
+      })
+
+      userEvent.click(filterModalButton)
+    }
+    it("should render a header", async () => {
+      render(
+        <FilterModalComponent
+          onFind={onFind}
+          onClear={onClear}
+          setSelectedPokemonType={setSelectedPokemonType}
+        />
+      )
+
+      clickButton()
+
+      const filterModalHeader = await screen.findByRole("banner")
+
+      expect(filterModalHeader).toBeInTheDocument()
+    })
+    it("should render a dropdown", async () => {
+      render(
+        <FilterModalComponent
+          onFind={onFind}
+          onClear={onClear}
+          setSelectedPokemonType={setSelectedPokemonType}
+        />
+      )
+
+      clickButton()
+
+      const dropdown = await screen.findByRole("button", { name: /fire/i })
+
+      expect(dropdown).toBeInTheDocument()
+    })
+    it("should render a footer", async () => {
+      render(
+        <FilterModalComponent
+          onFind={onFind}
+          onClear={onClear}
+          setSelectedPokemonType={setSelectedPokemonType}
+        />
+      )
+
+      clickButton()
+
+      const filterModalFooter = await screen.findByRole("contentinfo")
+
+      expect(filterModalFooter).toBeInTheDocument()
+    })
+  })
+})
