c6f54e389ffe3d10a08bd6ed69be1512e83144b0
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-09-05 11:46:35 +0200

    feat: fix test

--- src/components/dropdown/__tests__/dropdown.component.test.tsx
+++ src/components/dropdown/__tests__/dropdown.component.test.tsx
@@ -1,15 +1,22 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 import { DropdownComponent } from "../dropdown.component"
+import { PokemonTypesList } from "@/domain/pokemon/pokemon.types"
 
 describe("DropdownComponent", () => {
   const setSelectedPokemonType = jest.fn()
+  const selectedPokemonType = "grass"
 
   it("should render the button", async () => {
     render(
-      <DropdownComponent setSelectedPokemonType={setSelectedPokemonType} />
+      <DropdownComponent
+        setSelectedPokemonType={setSelectedPokemonType}
+        selectedPokemonType={PokemonTypesList[0].name}
+      />
     )
-    const dropdownButton = await screen.findByRole("button", { name: /fire/i })
+    const dropdownButton = await screen.findByRole("button", {
+      name: PokemonTypesList[0].name,
+    })
 
     expect(dropdownButton).toBeInTheDocument()
   })
