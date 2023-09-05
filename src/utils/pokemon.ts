e50560688207a4e21c30ecf68c8d4337e200f8d2
Javier <25299226+javiersanchezplanelles@users.noreply.github.com>
2023-09-05 22:41:24 +0200

    feat: add function

--- src/utils/pokemon.ts
+++ src/utils/pokemon.ts
@@ -1,7 +1,14 @@
-import { PokemonBase } from "@/domain/pokemon/pokemon.types"
+import { PokemonTypesList } from "@/domain/pokemon/pokemon.types"
 
 export const capitalizePokemonName = (pokemonName: string) =>
   `${pokemonName.charAt(0).toUpperCase()}${pokemonName.slice(1)}`
 
 export const getPokemonIndex = (pokemonId: number) =>
   ("000" + pokemonId).slice(-3)
+
+export const getPokemonColorType = (pokemonTypeName: string) => {
+  const pokemonType = PokemonTypesList.find(
+    (pokemonType) => pokemonType.name == pokemonTypeName
+  )
+  return pokemonType?.color
+}
